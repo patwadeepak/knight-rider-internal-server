9e0715e4e809ee2e24a37e9a440442dc6a5290b2
Deepak Kumar <deepak.kumar@balbix.com>
2025-10-22 07:08:18 +0530

    export Express methods by specifying type

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import type { NestExpressApplication } from '@nestjs/platform-express';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+async function bootsrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+
+bootsrap();
